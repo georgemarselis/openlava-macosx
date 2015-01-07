3e628a7eafee6a6fe75473311de86c0db5398ff1
George Marselis <george@marsel.is>
2015-01-07 02:43:56 +0200

    added files: one man file and two make .mk files, for daemons and liblsf, respectively

---
+++ lsf/daemons/daemons-sbin.mk
@@ -0,0 +1,20 @@
+vpath %.c eauthd limd mbatchd niosd resd pimd sbatchd
+
+
+daemons-sbin: eauthd limd mbatchd niosd resd pimd sbatchd
+
+eauthd:
+
+limd:
+
+mbatchd:
+
+niosd:
+
+resd:
+
+pimd:
+
+sbatchd:
+
+
