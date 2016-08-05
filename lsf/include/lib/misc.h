2aabb44d3a7701ba024165e4b8c2cef95a546fb2
George Marselis <george@marsel.is>
2016-08-05 05:04:23 +0300

    lsf/intlib/idx.y: fixed bison input

---
+++ lsf/include/lib/misc.h
@@ -0,0 +1,152 @@
+/* $Id: lib.conf.h 397 2007-11-26 19:04:00Z mblack $
+ * Copyright (C) 2007 Platform Computing Inc
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ */
+
+#pragma once
+ 
+#include "lsf.h"
+
+#define ILLEGAL_CHARS     ".!-=+*/[]@:&|{}'`\""
+#define M_THEN_A  1
+#define A_THEN_M  2
+#define M_OR_A    3
+#define TYPE1  RESF_BUILTIN | RESF_DYNAMIC | RESF_GLOBAL
+#define TYPE2  RESF_BUILTIN | RESF_GLOBAL
+#define TYPE3  RESF_BUILTIN | RESF_GLOBAL | RESF_LIC
+#define DEF_REXPRIORITY 0
+
+
+#define NL_SETN 42
+
+static struct lsInfo lsinfo;
+static struct clusterInfo clinfo;
+static struct sharedConf sConf_;
+static struct clusterConf cConf_;
+static struct sharedConf *sConf = NULL;
+static struct clusterConf *cConf = NULL;
+
+struct builtIn
+{
+    char *name;
+    char *des;
+    enum valueType valuetype;
+    enum orderType ordertype;
+    int flags;
+    int interval;
+}; 
+
+static struct builtIn builtInRes[24] = {
+    {"r15s",   "15-second CPU run queue length",             LS_NUMERIC, INCR, TYPE1 | RESF_RELEASE, 15},
+    {"r1m",    "1-minute CPU run queue length (alias: cpu)", LS_NUMERIC, INCR, TYPE1 | RESF_RELEASE, 15},
+    {"r15m",   "15-minute CPU run queue length",             LS_NUMERIC, INCR, TYPE1 | RESF_RELEASE, 15},
+    {"ut",     "1-minute CPU utilization (0.0 to 1.0)",      LS_NUMERIC, INCR, TYPE1, 15},
+    {"pg",     "Paging rate (pages/second)",                 LS_NUMERIC, INCR, TYPE1, 15},
+    {"io",     "Disk IO rate (Kbytes/second)",               LS_NUMERIC, INCR, TYPE1, 15},
+    {"ls",     "Number of login sessions (alias: login)",    LS_NUMERIC, INCR, TYPE1, 30},
+    {"it",     "Idle time (minutes) (alias: idle)",          LS_NUMERIC, DECR, TYPE1, 30},
+    {"tmp",    "Disk space in /tmp (Mbytes)",                LS_NUMERIC, DECR, TYPE1, 120},
+    {"swp",    "Available swap space (Mbytes) (alias: swap)",LS_NUMERIC, DECR, TYPE1, 15},
+    {"mem",    "Available memory (Mbytes)",                  LS_NUMERIC, DECR, TYPE1, 15},
+    {"ncpus",  "Number of CPUs",                             LS_NUMERIC, DECR, TYPE2, 0},
+    {"ndisks", "Number of local disks",                      LS_NUMERIC, DECR, TYPE2, 0},
+    {"maxmem", "Maximum memory (Mbytes)",                    LS_NUMERIC, DECR, TYPE2, 0},
+    {"maxswp", "Maximum swap space (Mbytes)",                LS_NUMERIC, DECR, TYPE2, 0},
+    {"maxtmp", "Maximum /tmp space (Mbytes)",                LS_NUMERIC, DECR, TYPE2, 0},
+    {"cpuf",   "CPU factor",                                 LS_NUMERIC, DECR, TYPE2, 0},
+    {"type",   "Host type",                                  LS_STRING,  NA,   TYPE2, 0},
+    {"model",  "Host model",                                 LS_STRING,  NA,   TYPE2, 0},
+    {"status", "Host status",                                LS_STRING,  NA,   TYPE2, 0},
+    {"rexpri", "Remote execution priority",                  LS_NUMERIC, NA,   TYPE2, 0},
+    {"server", "LSF server host",                            LS_BOOLEAN, NA,   TYPE2, 0},
+    {"hname",  "Host name",                                  LS_STRING,  NA,   TYPE2, 0},
+    {NULL,     NULL,                                         LS_NUMERIC, INCR, TYPE1, 0}
+};
+
+struct HostsArray
+{
+    uint size;
+    char padding[4];
+    char **hosts;
+};
+
+struct keymap {
+    int position;
+    char padding1[4];
+    char *key;
+    char *val;
+};
+
+static const int builtInRes_ID[] = {
+  1300, 1301, 1302, 1303, 1304, 1305, 1306, 1307, 1308, 1309,
+  1310, 1311, 1312, 1313, 1314, 1315, 1316, 1317, 1318, 1319,
+  1320, 1321
+};
+
+char do_Cluster     (FILE *fp, uint *lineNum, char *fname);
+char do_HostTypes   (FILE *fp, uint *lineNum, char *fname);
+char do_HostModels  (FILE *fp, uint *lineNum, char *fname);
+char do_Resources   (FILE *fp, uint *lineNum, char *fname);
+char do_Index       (FILE *fp, uint *lineNum, char *fname);
+char do_Manager     (FILE *fp, char *fname,   uint *lineNum, char *clustermanager, int lookupAdmins);
+char do_Hosts       (FILE *fp, char *fname,   uint *lineNum, struct lsInfo *myInfo);
+char do_Clparams    (FILE *fp, char *fname,   uint *lineNum);
+
+static char addHostType (char *);
+static char addHostModel (char *model, char *arch, double factor);
+static char setIndex    (struct keymap *keyList, char *fname, uint lineNum);
+char addHost     (struct hostInfo *host,  char *fname, uint *lineNum);
+
+int resNameDefined (char *);
+
+void initClusterInfo (struct clusterInfo *);
+void freeClusterInfo (struct clusterInfo *);
+void initHostInfo (struct hostInfo *);
+void freeHostInfo (struct hostInfo *);
+static void initResTable (void);
+static void putThreshold (int indx, struct hostEntry *hostEntryPtr, int position, char *val, float def);
+void liblsf_putThreshold (int indx, struct hostInfo *host, long position, char *val, float def);
+
+
+static int wgetClusAdmins (char *line, char *lsfile, uint *lineNum, char *secName);
+static struct admins *getAdmins_ (char *line, char *fname, uint *lineNum, char *secName, int lookupAdmins);
+struct admins *liblsf_getAdmins (char *line, char *fname, uint *lineNum, char *secName, int lookupAdmins);
+
+int parse_time (char *, float *, int *);
+int validWindow (char *, char *);
+
+static int setAdmins (struct admins *, int);
+static void freeKeyList (struct keymap *keyList);
+static int validType (char *type);
+int doResourceMap (FILE *fp, char *lsfile, uint *lineNum);
+static int addResourceMap (char *resName, char *location, char *lsfile, uint lineNum, int *isDefault);
+int liblsf_addResourceMap (char *resName, char *location, char *lsfile, uint lineNum);
+
+static uint parseHostList (char *hostList, char *lsfile, uint lineNum, char ***hosts, int *isDefault);
+int liblsf_parseHostList (char *hostList, char *lsfile, uint lineNum, char ***hosts);
+//  was : 
+// static struct lsSharedResourceInfo *addResource (char *resName, int nHosts, char **hosts, char *value, char *fileName, uint lineNum);
+struct lsSharedResourceInfo *liblsf_addResource (char *resName, int nHosts, char **hosts, char *value, char *fileName, uint lineNum);
+static struct sharedResource *addResource (char *resName, uint nHosts, char **hosts, char *value, char *fileName, uint lineNum, int resourceMap);
+int liblsf_addHostInstance(struct lsSharedResourceInfo *sharedResource, int nHosts, char **hostNames, char *value);
+static int addHostInstance (struct sharedResource *sharedResource, uint nHosts, char **hostNames, char *value, int resourceMap);
+
+int convertNegNotation_ (char **, struct HostsArray *);
+int resolveBaseNegHosts (char *, char **, struct HostsArray *);
+
+void freeSA_ (char **list, uint num);
+
+void doSkipSection(FILE *fp, uint *lineNum, char *lsfile, char *sectionName);
--- lsf/intlib/idx.y
+++ lsf/intlib/idx.y
@@ -1,6 +1,6 @@
 /* $Id: idx.y,v 1.1 2007/07/18 23:17:45 cchen Exp $
  ***********************************************************************
- *              Job Array Indcies  YYACC Parser 
+ *              Job Array indicies  YYACC Parser
  *                    -- IndParser --
  ***********************************************************************
  */
@@ -8,131 +8,131 @@
 %token SZZZZ    40
 
 %{
-#       include <stdio.h>
-#       include <stdlib.h>
-#       include "jidx.h"
-#	include "yparse.h"
-#       include "../lib/lsi18n.h" 
-#define NL_SETN      22      /* MOD_LSF_INTLIB  22  MsgId 550-599 */
+# include <stdio.h>
+# include <stdlib.h>
+#   include "libint/yparse.h"
+# include "libint/jidx.h"
+# include "libint/lsi18n.h"
+// MOD_LSF_INTLIB  22  MsgId 550-599 
+# define NL_SETN 22      
 
 #define YYALLOC(x) yyalloc(&idxAllocHead, (x))
 struct mallocList  *idxAllocHead = NULL;
-int     idxerrno = IDX_NOERR;
+int idxerrno = IDX_NOERR;
 
 %}
 
 %union  {
-        int ival ;
-        struct idxList  idxType;
-        struct idxList  *idxPtr;
-        }
+		int ival ;
+		struct idxList  idxType;
+		struct idxList  *idxPtr;
+		}
 
 
 %left ','
 %nonassoc '!'
 %right '='
 %left  OR
-%left '+' '-' '/' '*'   
+%left '+' '-' '/' '*'
 %right AND
 
 %type <idxType> index region
-%type <idxPtr> indcies arrayInd
+%type <idxPtr> indicies arrayInd
 %type <ival> icon
 %%
 
-arrayIndSpec   : arrayInd 
-               {
-                   *idxList = $1;                   
-                   yparseSucc(&idxAllocHead);
-                }
-                | arrayInd '%' icon
-                {
-                   *idxList = $1;
-                   *maxJLimit = $3;
-                    yparseSucc(&idxAllocHead);
-                }
-arrayInd	: '[' indcies ']'
+arrayIndSpec   : arrayInd
+			   {
+				   *idxList = $1;
+				   yparseSucc(&idxAllocHead);
+				}
+				| arrayInd '%' icon
+				{
+				   *idxList = $1;
+				   *maxJLimit = $3;
+					yparseSucc(&idxAllocHead);
+				}
+arrayInd    : '[' indicies ']'
 		{
-                   $$ = $2;
+				   $$ = $2;
 		}
-indcies		: index
-                {
-                   if (($$ = (struct idxList *) 
-                             YYALLOC(sizeof(struct idxList))) == NULL) {
-                       idxerror(_i18n_msg_get(ls_catd, NL_SETN, 550,
-					      "No Memory")); /* catgets 550 */
-                       idxerrno = IDX_MEM;
-                       YYABORT;
-                   }
-                   $$->start = $1.start;
-                   $$->end = $1.end;
-                   $$->step = $1.step;
-                   $$->next = NULL;
-                }
-                | index ',' indcies  
-                {
-                   if (($$ = (struct idxList *) 
-                             YYALLOC(sizeof(struct idxList))) == NULL) {
-                       idxerror(_i18n_msg_get(ls_catd, NL_SETN, 550, "No Memory"));
-                       idxerrno = IDX_MEM;
-                       YYABORT;
-                   }
-                   $$->start = $1.start;
-                   $$->end = $1.end;
-                   $$->step = $1.step;
-                   $$->next = $3;
-                }
-index          	: region 
-                {
-		    $$.start = $1.start;
-                    $$.end   = $1.end;
-                    $$.step  = 1;
-                    if ($$.start < 1 || $$.start > $$.end || $$.step <= 0) {
-                        idxerror(_i18n_msg_get(ls_catd, NL_SETN, 551,
-					       "boundary error")); /* catgets 551 */ 
-                        idxerrno = IDX_BOUND;
-                        YYABORT;
-                    } 
-                }
-                | region ':' icon
-                {
-		    $$.start = $1.start;
-                    $$.end   = $1.end;
-                    $$.step  = $3;
-                    if ($$.start < 1 || $$.start > $$.end || $$.step <= 0) {
-                        idxerror(_i18n_msg_get(ls_catd, NL_SETN, 551,
-					       "boundary error")); 
-                        idxerrno = IDX_BOUND;
-                        YYABORT;
-                    } 
-                }
-                
+indicies     : index
+				{
+				   if (($$ = (struct idxList *)
+							 YYALLOC(sizeof(struct idxList))) == NULL) {
+					   idxerror(_i18n_msg_get(ls_catd, NL_SETN, 550,
+						  "No Memory")); /* catgets 550 */
+					   idxerrno = IDX_MEM;
+					   YYABORT;
+				   }
+				   $$->start = $1.start;
+				   $$->end = $1.end;
+				   $$->step = $1.step;
+				   $$->next = NULL;
+				}
+				| index ',' indicies  
+				{
+				   if (($$ = (struct idxList *) 
+							 YYALLOC(sizeof(struct idxList))) == NULL) {
+					   idxerror(_i18n_msg_get(ls_catd, NL_SETN, 550, "No Memory"));
+					   idxerrno = IDX_MEM;
+					   YYABORT;
+				   }
+				   $$->start = $1.start;
+				   $$->end = $1.end;
+				   $$->step = $1.step;
+				   $$->next = $3;
+				}
+index           : region
+				{
+			$$.start = $1.start;
+					$$.end   = $1.end;
+					$$.step  = 1;
+					if ($$.start < 1 || $$.start > $$.end || $$.step <= 0) {
+						/* catgets 551 */ 
+						idxerror( "551: boundary error" ); 
+						idxerrno = IDX_BOUND;
+						YYABORT;
+					} 
+				}
+				| region ':' icon
+				{
+			$$.start = $1.start;
+					$$.end   = $1.end;
+					$$.step  = $3;
+					if ($$.start < 1 || $$.start > $$.end || $$.step <= 0) {
+						idxerror(_i18n_msg_get(ls_catd, NL_SETN, 551,
+						   "boundary error")); 
+						idxerrno = IDX_BOUND;
+						YYABORT;
+					}
+				}
 region          : icon
-                {
-                    $$.start = $1;
-                    $$.end  = $$.start;
-                    $$.step = 1;   
-                };
-                | '-' icon
-                {
-                    $$.start = 1;
-                    $$.end  = $2;
-                    $$.step = 1;
-                };
-                | icon '-' 
-                {
-                    $$.start = $1; 
-                    $$.end  = INFINIT_INT;
-                    $$.step = 1;
-                };
-                | icon '-' icon 
-                {
-                    $$.start = $1;
-                    $$.end  = $3;
-                    $$.step = 1;
-  		}
-icon		: ICON
+				{
+					$$.start = $1;
+					$$.end  = $$.start;
+					$$.step = 1;
+				};
+				| '-' icon
+				{
+					$$.start = 1;
+					$$.end  = $2;
+					$$.step = 1;
+				};
+				| icon '-'
+				{
+					$$.start = $1;
+					$$.end  = INFINIT_INT;
+					$$.step = 1;
+				};
+				| icon '-' icon
+				{
+					$$.start = $1;
+					$$.end  = $3;
+					$$.step = 1;
+				}
+icon        : ICON
 		{
-		    $$ = atoi(token);
+			$$ = atoi(token);
 		}
 %%
