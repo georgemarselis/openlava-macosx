5a491e04e03a71cba2905c28ff099ef3623e6e00
George Marselis <george@marsel.is>
2018-08-14 04:34:04 +0300

    include/lsb/conf.h: removed static keyword from variables. Let's find out where all the multi-declerations happen

--- lsf/include/lsb/conf.h
+++ lsf/include/lsb/conf.h
@@ -48,33 +48,33 @@
     char *prf_level;
 };
 
-static struct paramConf *pConf      = NULL;
-static struct userConf *uConf       = NULL;
-static struct hostConf *hConf       = NULL;
-static struct queueConf *qConf      = NULL;
-static struct userInfoEnt **users   = NULL;
-static struct hostInfoEnt **hosts   = NULL;
-static struct queueInfoEnt **queues = NULL;
+struct paramConf *pConf      = NULL;
+struct userConf *uConf       = NULL;
+struct hostConf *hConf       = NULL;
+struct queueConf *qConf      = NULL;
+struct userInfoEnt **users   = NULL;
+struct hostInfoEnt **hosts   = NULL;
+struct queueInfoEnt **queues = NULL;
 
-static struct groupInfoEnt *usergroups[MAX_GROUPS];
-static struct groupInfoEnt *hostgroups[MAX_GROUPS];
+struct groupInfoEnt *usergroups[MAX_GROUPS];
+struct groupInfoEnt *hostgroups[MAX_GROUPS];
 
-static struct hTab unknownUsers;
+struct hTab unknownUsers;
 // static struct clusterConf cConf_;
 // static struct sharedConf sConf_;
 
-static unsigned int numofugroups = 0;
-static unsigned int numofhosts   = 0;
-static unsigned int numofhgroups = 0;
-static unsigned int numofqueues  = 0;
-static unsigned int usersize     = 0;
-static unsigned int hostsize     = 0;
-static unsigned int queuesize    = 0;
-static unsigned int numofusers   = 0;
-static double maxFactor  = 0.0;
-static char *maxHName    = NULL;
+unsigned int numofugroups = 0;
+unsigned int numofhosts   = 0;
+unsigned int numofhgroups = 0;
+unsigned int numofqueues  = 0;
+unsigned int usersize     = 0;
+unsigned int hostsize     = 0;
+unsigned int queuesize    = 0;
+unsigned int numofusers   = 0;
+double maxFactor  = 0.0;
+char *maxHName    = NULL;
 
-static bool_t initUnknownUsers = FALSE;
+bool_t initUnknownUsers = FALSE;
 
 
 char *getNextWord1_ (char **);
@@ -82,79 +82,79 @@ char *getNextWord1_ (char **);
 // #define TYPE1  RESF_BUILTIN | RESF_DYNAMIC | RESF_GLOBAL
 // #define TYPE2  RESF_BUILTIN | RESF_GLOBAL
 
-static char do_Param  (struct lsConf *conf, char *fname, size_t *lineNumber);
-static char do_Users  (struct lsConf *conf, char *fname, size_t *lineNumber, int option);
-static char do_Hosts_ (struct lsConf *conf, char *fname, size_t *lineNumber, struct lsInfo *info, int options);
-static char do_Queues (struct lsConf *conf, char *fname, size_t *lineNumber, struct lsInfo *info, int options);
-static char do_Groups (struct groupInfoEnt **groups, struct lsConf *conf, char *fname, size_t *lineNumber, unsigned int *ngroups, int options);
-static int  isHostName (char *grpName);
-static int  addHostEnt (struct hostInfoEnt *hp, struct hostInfo *hostInfo, size_t *override);
-static char addQueue  (struct queueInfoEnt *qp, char *fname, unsigned int lineNum);
-static char addUser   (char *, int, float, char *, int);
-static char addMember (struct groupInfoEnt *gp, char *word, int grouptype, char *fname, unsigned int lineNumber, char *section, int options, int checkAll);
+char do_Param  (struct lsConf *conf, char *fname, size_t *lineNumber);
+char do_Users  (struct lsConf *conf, char *fname, size_t *lineNumber, int option);
+char do_Hosts_ (struct lsConf *conf, char *fname, size_t *lineNumber, struct lsInfo *info, int options);
+char do_Queues (struct lsConf *conf, char *fname, size_t *lineNumber, struct lsInfo *info, int options);
+char do_Groups (struct groupInfoEnt **groups, struct lsConf *conf, char *fname, size_t *lineNumber, unsigned int *ngroups, int options);
+int  isHostName (char *grpName);
+int  addHostEnt (struct hostInfoEnt *hp, struct hostInfo *hostInfo, size_t *override);
+char addQueue  (struct queueInfoEnt *qp, char *fname, unsigned int lineNum);
+char addUser   (char *, int, float, char *, int);
+char addMember (struct groupInfoEnt *gp, char *word, int grouptype, char *fname, unsigned int lineNumber, char *section, int options, int checkAll);
 
-static char isInGrp (char *, struct groupInfoEnt *, int, int);
-static char **expandGrp (char *, unsigned int *, int);
-static struct groupInfoEnt *addGroup (struct groupInfoEnt **, char *, unsigned int *, int);
+char isInGrp (char *, struct groupInfoEnt *, int, int);
+char **expandGrp (char *, unsigned int *, int);
+struct groupInfoEnt *addGroup (struct groupInfoEnt **, char *, unsigned int *, int);
 
-static struct groupInfoEnt *addUnixGrp (struct group *, char *, char *, unsigned int, char *, int);
-static char *parseGroups (char *linep, char *fname, size_t *lineNumber, char *section, int groupType, int options);
+struct groupInfoEnt *addUnixGrp (struct group *, char *, char *, unsigned int, char *, int);
+char *parseGroups (char *linep, char *fname, size_t *lineNumber, char *section, int groupType, int options);
 
-static struct groupInfoEnt *getUGrpData (char *);
-static struct groupInfoEnt *getHGrpData (char *);
-static struct groupInfoEnt *getGrpData (struct groupInfoEnt **groups, char *name, unsigned int num);
-static struct userInfoEnt  *getUserData (char *);
-static struct hostInfoEnt  *getHostData (char *);
-static struct queueInfoEnt *getQueueData (char *);
+struct groupInfoEnt *getUGrpData (char *);
+struct groupInfoEnt *getHGrpData (char *);
+struct groupInfoEnt *getGrpData (struct groupInfoEnt **groups, char *name, unsigned int num);
+struct userInfoEnt  *getUserData (char *);
+struct hostInfoEnt  *getHostData (char *);
+struct queueInfoEnt *getQueueData (char *);
 
-static void initParameterInfo (struct parameterInfo *);
-static void freeParameterInfo (struct parameterInfo *);
-static void initUserInfo  (struct userInfoEnt *);
-static void freeUserInfo  (struct userInfoEnt *);
-static void initGroupInfo (struct groupInfoEnt *);
-static void freeGroupInfo (struct groupInfoEnt *);
-static void initHostInfoEnt (struct hostInfoEnt *);
-static void freeHostInfoEnt  (struct hostInfoEnt *);
-static void initQueueInfo (struct queueInfoEnt *);
-static void freeQueueInfo (struct queueInfoEnt *);
+void initParameterInfo (struct parameterInfo *);
+void freeParameterInfo (struct parameterInfo *);
+void initUserInfo  (struct userInfoEnt *);
+void freeUserInfo  (struct userInfoEnt *);
+void initGroupInfo (struct groupInfoEnt *);
+void freeGroupInfo (struct groupInfoEnt *);
+void initHostInfoEnt (struct hostInfoEnt *);
+void freeHostInfoEnt  (struct hostInfoEnt *);
+void initQueueInfo (struct queueInfoEnt *);
+void freeQueueInfo (struct queueInfoEnt *);
 
 
-static void initThresholds (struct lsInfo *, float *, float *);
-static void getThresh (struct lsInfo *info, struct keymap *keyList, float *loadSched, float *loadStop, char *fname, size_t *lineNumber, char *description);
+void initThresholds (struct lsInfo *, float *, float *);
+void getThresh (struct lsInfo *info, struct keymap *keyList, float *loadSched, float *loadStop, char *fname, size_t *lineNumber, char *description);
 
-static char searchAll (char *);
+char searchAll (char *);
 
-static void checkCpuLimit (char **hostSpec, double **cpuFactor, int useSysDefault, char *fname, size_t *lineNumber, char *pname, struct lsInfo *info, int options);
-static int parseDefAndMaxLimits (struct keymap key, int *defaultVal, int *maxVal, char *fname, size_t *lineNumber, char *pname);
-static int parseCpuAndRunLimit (struct keymap *keylist, struct queueInfoEnt *qp, char *fname, size_t *lineNumber, char *pname, struct lsInfo *info, int options);
-static int parseProcLimit (char *word, struct queueInfoEnt *qp, char *fname, size_t *lineNumber, char *pname);
-static int parseLimitAndSpec (char *word, int *limit, char **spec, char *hostSpec, char *param, struct queueInfoEnt *qp, char *fname, size_t *lineNumber, char *pname);
-static int my_atoi (char *, int, int);
-static float my_atof (char *, float, float);
-static void resetUConf (struct userConf *);
-static int copyHostInfo (struct hostInfoEnt *, struct hostInfoEnt *);
-static void resetHConf (struct hostConf *hConf);
-static double *getModelFactor (char *, struct lsInfo *);
-static double *getHostFactor (char *);
-static char *parseAdmins (char *admins, int options, char *fname, size_t *lineNum);
-static char *putIntoList (char **list, unsigned int *len, char *string, char *listName);
-static int isInList (char *, char *);
-static int setDefaultHost (struct lsInfo *);
-static int handleUserMem (void);
-static int setDefaultUser (void);
-static int handleHostMem (void);
-static void freeSA (char **list, unsigned int num);
-static int checkAllOthers (char *, int *);
-static int resolveBatchNegHosts (char *, char **, int);
-static unsigned int fillCell_ (struct inNames **table, char *name, char *level);
-static unsigned int expandWordAll (unsigned int *size, unsigned int *num, struct inNames **inTable, char *ptr_level);
-static int readHvalues_conf (struct keymap *keyList, char *linep, struct lsConf *conf, char *lsfile, size_t *lineNumber, int exact, char *section);
-static char *a_getNextWord_ (char **);
-static int getReserve (char *reserve, struct queueInfoEnt *qp, char *filename, unsigned int lineNum);
-static int isServerHost (char *);
+void checkCpuLimit (char **hostSpec, double **cpuFactor, int useSysDefault, char *fname, size_t *lineNumber, char *pname, struct lsInfo *info, int options);
+int parseDefAndMaxLimits (struct keymap key, int *defaultVal, int *maxVal, char *fname, size_t *lineNumber, char *pname);
+int parseCpuAndRunLimit (struct keymap *keylist, struct queueInfoEnt *qp, char *fname, size_t *lineNumber, char *pname, struct lsInfo *info, int options);
+int parseProcLimit (char *word, struct queueInfoEnt *qp, char *fname, size_t *lineNumber, char *pname);
+int parseLimitAndSpec (char *word, int *limit, char **spec, char *hostSpec, char *param, struct queueInfoEnt *qp, char *fname, size_t *lineNumber, char *pname);
+int my_atoi (char *, int, int);
+float my_atof (char *, float, float);
+void resetUConf (struct userConf *);
+int copyHostInfo (struct hostInfoEnt *, struct hostInfoEnt *);
+void resetHConf (struct hostConf *hConf);
+double *getModelFactor (char *, struct lsInfo *);
+double *getHostFactor (char *);
+char *parseAdmins (char *admins, int options, char *fname, size_t *lineNum);
+char *putIntoList (char **list, unsigned int *len, char *string, char *listName);
+int isInList (char *, char *);
+int setDefaultHost (struct lsInfo *);
+int handleUserMem (void);
+int setDefaultUser (void);
+int handleHostMem (void);
+void freeSA (char **list, unsigned int num);
+int checkAllOthers (char *, int *);
+int resolveBatchNegHosts (char *, char **, int);
+unsigned int fillCell_ (struct inNames **table, char *name, char *level);
+unsigned int expandWordAll (unsigned int *size, unsigned int *num, struct inNames **inTable, char *ptr_level);
+int readHvalues_conf (struct keymap *keyList, char *linep, struct lsConf *conf, char *lsfile, size_t *lineNumber, int exact, char *section);
+char *a_getNextWord_ (char **);
+int getReserve (char *reserve, struct queueInfoEnt *qp, char *filename, unsigned int lineNum);
+int isServerHost (char *);
 
-static void addBinaryAttributes (char *confFile, size_t *lineNumber, struct queueInfoEnt *queue, struct keymap *keylist, unsigned int attrib, char *attribName);
-static int parseQFirstHost (char *myWord, int *haveFirst, char *pname, size_t *lineNumber, char *fname, char *section);
+void addBinaryAttributes (char *confFile, size_t *lineNumber, struct queueInfoEnt *queue, struct keymap *keylist, unsigned int attrib, char *attribName);
+int parseQFirstHost (char *myWord, int *haveFirst, char *pname, size_t *lineNumber, char *fname, char *section);
 
 void freeUConf (struct userConf *, int);
 void freeHConf (struct hostConf *, int);
