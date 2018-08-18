bc1ee3a12d6a1f64aa2ebb722ff0ed8c25517fbf
George Marselis <george@marsel.is>
2018-08-18 13:55:34 +0300

    include/cmdtools/read.event.h : header for cmdtools/lib/read.event.c

---
+++ lsf/include/cmdtools/read.event.h
@@ -0,0 +1,20 @@
+// added by George Marselis <george@marsel.is
+
+#pragma once
+
+// #define NL_SETN  6
+#define GET_JOBID(jobId, idx) ((Req.options & OPT_ARRAY_INFO)) ? (jobId): LSB_JOBID((jobId), (idx))
+
+// extern void initTab (struct hTab *tabPtr);
+// extern hEnt *addMemb (struct hTab *tabPtr, LS_LONG_INT member);
+// extern char remvMemb (struct hTab *tabPtr, LS_LONG_INT member);
+// extern int matchName (char *, char *);
+
+void inJobList (struct jobRecord *pred, struct jobRecord *entry);
+void offJobList (struct jobRecord *entry);
+void insertModEvent (struct eventRec *log, hEnt * ent);
+struct jobRecord *createJobRec (int);
+
+// extern struct jobRecord *jobRecordList;
+// extern struct eventLogHandle *eLogPtr;
+// extern float version;
