d95ceea79ebf4796ace2707c1a0022fc9e4f2880
George Marselis <george.marselis@ncmm.uio.no>
2019-03-27 23:55:21 +0100

    cmdtools/bcheckpoint/echkpnt.c: formatted

--- lsf/cmdtools/bcheckpoint/echkpnt.c
+++ lsf/cmdtools/bcheckpoint/echkpnt.c
@@ -27,17 +27,13 @@
 #include "echkpnt.lib.h"
 #include "echkpnt.env.h"
 
-#define LSTMPDIR        lsTmpDir_
-
-void echkpnt_usage (const char *);
 
 static char logMesgBuf[MAX_LINE_LEN];
-extern char *lsTmpDir_;
 
-static void
+void
 echkpnt_usage (const char *pCmd)
 {
-  fprintf (stderr, "Usage: %s [-c] [-f] [-k | -s] [-x] [-d chkdir] [-h] [-V] pid\n", Cmd);
+    fprintf (stderr, "Usage: %s [-c] [-f] [-k | -s] [-x] [-d chkdir] [-h] [-V] pid\n", Cmd);
 }
 
 
@@ -45,337 +41,292 @@ int
 main (int argc, char **argv)
 {
 
-  static char __func__] = "main()";
+  // static char __func__] = "main()";
+    char *pMethodName = NULL;
+    char *pMethodDir = NULL;
+    char *pIsOutput = NULL;
+    char *pJobID = NULL;
+
+    char *presAcctFN = NULL;
+    char resAcctFN[MAX_FILENAME_LEN];
+
+    char echkpntProgPath[MAX_PATH_LEN];
+    char *pChkpntDir = NULL;
+    pid_t iChildPid;
+    LS_WAIT_T iStatus;
+    char *cargv[MAX_ARGS];
+    int argIndex = 0;
+    int cc = 0;
+    int iReValue = 0;
+    char *jf = NULL
+    char *jfn = NULL;
 
 
-  char *pMethodName = NULL;
-  char *pMethodDir = NULL;
-  char *pIsOutput = NULL;
-  char *pJobID = NULL;
-
-  char *presAcctFN = NULL;
-  char resAcctFN[MAX_FILENAME_LEN];
-
-  char echkpntProgPath[MAX_PATH_LEN];
-  char *pChkpntDir = NULL;
-  pid_t iChildPid;
-  LS_WAIT_T iStatus;
-  char *cargv[MAX_ARGS];
-  int argIndex = 0;
-  int cc;
-  int iReValue;
-  char *jf, *jfn;
-
-
-  while ((cc = getopt (argc, argv, "cfksd:Vhx")) != EOF)
-    {
-      switch (cc)
-	{
-	case 'c':
-	case 'f':
-	case 'k':
-	case 'x':
-	case 's':
-	  break;
-	case 'V':
-	  fputs (_LS_VERSION_, stderr);
-	  exit (0);
-	case 'd':
-	  pChkpntDir = optarg;
-	  break;
-	case 'h':
-	  usage (argv[0]);
-	  exit (0);
-	default:
-	  usage (argv[0]);
-	  exit (-1);
-	}
+    while ((cc = getopt (argc, argv, "cfksd:Vhx")) != EOF)  {
+        switch (cc) {
+            case 'c': // W
+            case 'f': // T
+            case 'k': // F
+            case 'x': // are all these switches supposed to be doing
+            case 's': // 
+                {
+                    1; // I don't know man, don't ask me
+                }
+            break;
+            case 'V': {
+                fprintf( STDERR, _LS_VERSION_, stderr);
+                exit (0); 
+            }
+            case 'd':
+                pChkpntDir = optarg;
+            break
+            break;
+            case 'h':
+                usage (argv[0]);
+                exit (0);
+            break;
+            default: {
+                usage (argv[0]);
+                exit (-1); // FIXME FIXME FIXME FIXME replace negative number with positive, but no idea what it is supposed to mean
+            }
+            break;
+        }
     }
 
 
-  if (pChkpntDir == NULL)
-    {
-      usage (argv[0]);
-      exit (-1);
+    if (pChkpntDir == NULL) {
+        fprintf( stderr, "No checkpoint directory defined.");
+        usage (argv[0]);
+        exit (-1);
     }
 
-  if (access (pChkpntDir, W_OK | X_OK) < 0)
-    {
-      fprintf (stderr, "%s : the chkpnt dir %s can not be accessed\n",
-	       __func__, pChkpntDir);
-      exit (-1);
+    if (access (pChkpntDir, W_OK | X_OK) < 0) {
+        fprintf (stderr, "%s : the chkpnt dir %s can not be accessed\n", __func__, pChkpntDir);
+        exit (-1);
     }
 
+    iReValue = fileIsExist (pChkpntDir);
 
-  iReValue = fileIsExist (pChkpntDir);
+    if (iReValue == 1) {
+        pMethodName = getEchkpntVar (ECHKPNT_METHOD);
 
-  if (iReValue == 1)
-    {
+        if ((pMethodName == NULL) || (strcmp (pMethodName, "") == 0)) {
+            pMethodName = ECHKPNT_DEFAULT_METHOD;
+        }
 
-
-      pMethodName = getEchkpntVar (ECHKPNT_METHOD);
-
-      if ((pMethodName == NULL) || (strcmp (pMethodName, "") == 0))
-	{
-	  pMethodName = ECHKPNT_DEFAULT_METHOD;
-	}
-
-      pMethodDir = getEchkpntVar (ECHKPNT_METHOD_DIR);
-      pIsOutput = getEchkpntVar (ECHKPNT_KEEP_OUTPUT);
-
+        pMethodDir = getEchkpntVar (ECHKPNT_METHOD_DIR);
+        pIsOutput = getEchkpntVar (ECHKPNT_KEEP_OUTPUT);
     }
-  else if (iReValue == 0)
-    {
+    else if (iReValue == 0) {
 
+        initenv_ (NULL, NULL);
+        pMethodName = getenv (ECHKPNT_METHOD);
 
-      initenv_ (NULL, NULL);
-      pMethodName = getenv (ECHKPNT_METHOD);
+        if ((pMethodName == NULL) || (strcmp (pMethodName, "") == 0)) {
+            pMethodName = ECHKPNT_DEFAULT_METHOD;
+        }
+        pMethodDir = getenv (ECHKPNT_METHOD_DIR);
+        pIsOutput = getenv (ECHKPNT_KEEP_OUTPUT);
 
-      if ((pMethodName == NULL) || (strcmp (pMethodName, "") == 0))
-	{
-	  pMethodName = ECHKPNT_DEFAULT_METHOD;
-	}
-      pMethodDir = getenv (ECHKPNT_METHOD_DIR);
-      pIsOutput = getenv (ECHKPNT_KEEP_OUTPUT);
+        jf = getenv ("LSB_JOBFILENAME"); // FIXME FIXME FIXME FIXME put this in configure.ac
+        if (jf == NULL) {
+            fprintf (stderr, "%s : getenv of LSB_JOBFILENAME failed", __func__);
+        }
+        else {
+            jfn = strchr (jf, '/'); // FIXME FIXME FIXME FIXME put this in configure.ac
+            if (jfn) {
+                sprintf (resAcctFN, "%s/.%s.acct", LSTMPDIR, jfn + 1); // FIXME FIXME FIXME FIXME put this in configure.ac
+                presAcctFN = (char *) resAcctFN;
+            }
+        }
 
-      {
-
-	jf = getenv ("LSB_JOBFILENAME");
-	if (jf == NULL)
-	  {
-	    fprintf (stderr, "%s : getenv of LSB_JOBFILENAME failed", __func__);
-	  }
-	else
-	  {
-	    jfn = strchr (jf, '/');
-	    if (jfn)
-	      {
-		sprintf (resAcctFN, "%s/.%s.acct", LSTMPDIR, jfn + 1);
-		presAcctFN = (char *) resAcctFN;
-	      }
-	  }
-      }
-
-      writeEchkpntVar (ECHKPNT_METHOD, pMethodName);
-      writeEchkpntVar (ECHKPNT_METHOD_DIR, pMethodDir);
-      writeEchkpntVar (ECHKPNT_KEEP_OUTPUT, pIsOutput);
-      writeEchkpntVar (ECHKPNT_ACCT_FILE, presAcctFN);
+        writeEchkpntVar (ECHKPNT_METHOD, pMethodName);
+        writeEchkpntVar (ECHKPNT_METHOD_DIR, pMethodDir);
+        writeEchkpntVar (ECHKPNT_KEEP_OUTPUT, pIsOutput);
+        writeEchkpntVar (ECHKPNT_ACCT_FILE, presAcctFN);
     }
-  else
-    {
-      fprintf (stderr, "%s : the .echkpnt file content occurs error: %s\n",
-	       __func__, strerror (errno));
-      exit (-1);
+    else {
+        fprintf (stderr, "%s : the .echkpnt file content occurs error: %s\n", __func__, strerror (errno)); // FIXME FIXME FIXME FIXME put  .echkpnt  in configure.ac
+        exit (-1); // FIXME FIXME FIXME FIXME replace negative number with positive indicating the .echkpnt file content occurs error
     }
 
+    if( ( pIsOutput != NULL ) && ( ( strcmp (pIsOutput, ECHKPNT_OPEN_OUTPUT) == 0 ) || ( strcmp (pIsOutput, ECHKPNT_OPEN_OUTPUT_L) == 0 ) ) ) {
+        initLog ("Echkpnt"); // FIXME FIXME FIXME FIXME put this in configure.ac
+    }
 
-  if ((pIsOutput != NULL) && ((strcmp (pIsOutput, ECHKPNT_OPEN_OUTPUT) == 0)
-			      || (strcmp (pIsOutput, ECHKPNT_OPEN_OUTPUT_L) ==
-				  0)))
-    {
-      initLog ("Echkpnt");
+#ifdef DEBUG // FIXME FIXME FIXME FIXME replace with run-time debug
+    sprintf (logMesgBuf, "%s : the LSB_ECHKPNT_METHOD = %s\n", __func__, pMethodName);
+    logMesg (logMesgBuf);
+    sprintf (logMesgBuf, "%s : the LSB_ECHKPNT_METHOD_DIR = %s\n", __func__, pMethodDir != NULL ? pMethodDir : "");
+    logMesg (logMesgBuf);
+#endif
+
+    if (getEchkpntMethodDir( echkpntProgPath, pMethodDir, ECHKPNT_PROGRAM, pMethodName ) == NULL ) {
+        sprintf (logMesgBuf, "%s : the echkpnt method(%s) path is not correct\n", __func__, pMethodName);
+        // goto Error;
+        fprintf (stderr, "%s", logMesgBuf);
+        logMesg (logMesgBuf);
+        freeTable_ ();
+        closeLog ();
+        exit (-1);
     }
 
 #ifdef DEBUG
-  sprintf (logMesgBuf, "%s : the LSB_ECHKPNT_METHOD = %s\n", __func__,
-	   pMethodName);
-  logMesg (logMesgBuf);
-  sprintf (logMesgBuf, "%s : the LSB_ECHKPNT_METHOD_DIR = %s\n", __func__,
-	   pMethodDir != NULL ? pMethodDir : "");
-  logMesg (logMesgBuf);
+    sprintf (logMesgBuf, "%s : the echkpntProgPath is : %s\n", __func__, echkpntProgPath);
+    logMesg (logMesgBuf);
 #endif
 
-
-  if (getEchkpntMethodDir
-      (echkpntProgPath, pMethodDir, ECHKPNT_PROGRAM, pMethodName) == NULL)
-    {
-      sprintf (logMesgBuf,
-	       "%s : the echkpnt method(%s) path is not correct\n", __func__,
-	       pMethodName);
-      goto Error;
+    for (argIndex = 0; argIndex < argc; argIndex++) {
+        cargv[argIndex] = argv[argIndex];
     }
+    cargv[argIndex] = NULL;
+
+    if(  strcmp( pMethodName, ECHKPNT_DEFAULT_METHOD ) == 0 ) {
+        cargv[0] = "echkpnt.default"; // FIXME FIXME FIXME FIXME put .echkpnt.default in configure.ac
+        freeTable_ ();
 
 #ifdef DEBUG
-  sprintf (logMesgBuf, "%s : the echkpntProgPath is : %s\n", __func__,
-	   echkpntProgPath);
-  logMesg (logMesgBuf);
+        logMesg ("the echkpnt.default will be executed\n");
 #endif
-
-  for (argIndex = 0; argIndex < argc; argIndex++)
-    {
-      cargv[argIndex] = argv[argIndex];
-    }
-  cargv[argIndex] = NULL;
-
-
-  if (strcmp (pMethodName, ECHKPNT_DEFAULT_METHOD) == 0)
-    {
-      cargv[0] = "echkpnt.default";
-      freeTable_ ();
-
-#ifdef DEBUG
-      logMesg ("the echkpnt.default will be executed\n");
-#endif
-      closeLog ();
-      execv (echkpntProgPath, cargv);
-      sprintf (logMesgBuf, "%s : execute the echkpnt.default fail\n%s\n",
-	       __func__, (errno ? strerror (errno) : ""));
-      fprintf (stderr, "%s", logMesgBuf);
-      exit (-1);
+        closeLog ();
+        execv (echkpntProgPath, cargv); // we are not supposed to come back from here
+        sprintf (logMesgBuf, "%s : execute the echkpnt.default fail\n%s\n",  __func__, (errno ? strerror (errno) : "")); // FIXME FIXME FIXME FIXME put .echkpnt.default in configure.ac
+        fprintf (stderr, "%s", logMesgBuf);
+        exit (-1); // FIXME FIXME FIXME FIXME replace negative value with value indicating failure to execv
     }
 
-
-  iChildPid = fork ();
-  if (iChildPid < 0)
-    {
-      sprintf (logMesgBuf, "%s : fork() fork a child process fail...\n",
-	       __func__);
-      goto Error;
-
-
+    iChildPid = fork ();
+    if (iChildPid < 0) {
+        sprintf (logMesgBuf, "%s : fork() fork a child process fail...\n",  __func__);
+        // goto Error;
+        fprintf (stderr, "%s", logMesgBuf);
+        logMesg (logMesgBuf);
+        freeTable_ ();
+        closeLog ();
+        exit (-1); // FIXME FIXME FIXME FIXME replace negative value with value indicating failure to fork
     }
-  else if (iChildPid == 0)
-    {
-      long lMaxfds;
-      int ii;
-      char progName[MAX_FILENAME_LEN];
+    else if (iChildPid == 0) {
+
+        // int ii = 0;
+        long lMaxfds = 0;
+        char progName[MAX_FILENAME_LEN];
+
+        memset( progName, '\0', MAX_FILENAME_LEN );
+
+        sprintf (logMesgBuf, "erestart.%s", pMethodName); // FIXME FIXME FIXME FIXME put .echkpnt.default in configure.ac
+        setMesgHeader (logMesgBuf);
 
 
-      sprintf (logMesgBuf, "erestart.%s", pMethodName);
-      setMesgHeader (logMesgBuf);
+        if ((pIsOutput == NULL) || ((strcmp (pIsOutput, ECHKPNT_OPEN_OUTPUT) != 0)  && (strcmp (pIsOutput, ECHKPNT_OPEN_OUTPUT_L) != 0))) {
+            if (redirectFd (ECHKPNT_DEFAULT_OUTPUT_FILE, 1) == -1) { // FIXME FIXME FIXME what is this 1? put an enum in
+                sprintf (logMesgBuf, "%s : redirect stdout to %s file\n%s\n", __func__, ECHKPNT_DEFAULT_OUTPUT_FILE, errno ? strerror (errno) : "");
+                // goto Error;
+                fprintf (stderr, "%s", logMesgBuf);
+                logMesg (logMesgBuf);
+                freeTable_ ();
+                closeLog ();
+                exit (-1); // FIXME FIXME FIXME FIXME replace negative value with value indicating failure to redirectFd 1
+            }
+            if (redirectFd (ECHKPNT_DEFAULT_OUTPUT_FILE, 2) == -1) { // FIXME FIXME FIXME what is this 2? put an enum in
+                sprintf (logMesgBuf, "%s : redirect stderr to %s file\n%s\n", __func__, ECHKPNT_DEFAULT_OUTPUT_FILE, errno ? strerror (errno) : "");
+                // goto Error;
+                fprintf (stderr, "%s", logMesgBuf);
+                logMesg (logMesgBuf);
+                freeTable_ ();
+                closeLog ();
+                exit (-1); // FIXME FIXME FIXME FIXME replace negative value with value indicating failure to redirectFd 2
+            }
+        }
+        else {
+            char aFileName[MAX_PATH_LEN];
+            if( ( getChkpntDirFile( aFileName, ECHKPNT_STDOUT_FILE ) == -1 ) || ( redirectFd ( aFileName, 1 ) == -1 ) ) { // FIXME FIXME FIXME what is this redirectFd 1? put an enum in
+                sprintf (logMesgBuf, "%s : redirect the stdout to %s fail\n", __func__, ECHKPNT_STDOUT_FILE); 
+                logMesg (logMesgBuf);
 
+                if( redirectFd( ECHKPNT_DEFAULT_OUTPUT_FILE, 1 ) == -1 ) { // FIXME FIXME FIXME what is this redirectFd 1? put an enum in
+                    sprintf (logMesgBuf, "%s : redirect stdout to %s file fail\n%s\n",  __func__, ECHKPNT_DEFAULT_OUTPUT_FILE, errno ? strerror (errno) : "");
+                // goto Error;
+                fprintf (stderr, "%s", logMesgBuf);
+                logMesg (logMesgBuf);
+                freeTable_ ();
+                closeLog ();
+                exit (-1); // FIXME FIXME FIXME FIXME replace negative value with value indicating failure to redirectFd 1
+                }
+            }
 
-      if ((pIsOutput == NULL)
-	  || ((strcmp (pIsOutput, ECHKPNT_OPEN_OUTPUT) != 0)
-	      && (strcmp (pIsOutput, ECHKPNT_OPEN_OUTPUT_L) != 0)))
-	{
-	  if (redirectFd (ECHKPNT_DEFAULT_OUTPUT_FILE, 1) == -1)
-	    {
-	      sprintf (logMesgBuf, "%s : redirect stdout to %s file\n%s\n",
-		       __func__, ECHKPNT_DEFAULT_OUTPUT_FILE,
-		       errno ? strerror (errno) : "");
-	      goto Error;
-	    }
-	  if (redirectFd (ECHKPNT_DEFAULT_OUTPUT_FILE, 2) == -1)
-	    {
-	      sprintf (logMesgBuf, "%s : redirect stderr to %s file\n%s\n",
-		       __func__, ECHKPNT_DEFAULT_OUTPUT_FILE,
-		       errno ? strerror (errno) : "");
-	      goto Error;
-	    }
+            if ((getChkpntDirFile (aFileName, ECHKPNT_STDERR_FILE) == -1) || (redirectFd (aFileName, 2) == -1)) {
+                sprintf (logMesgBuf, "%s : redirect the stderr to %s fail\n", __func__, ERESTART_STDERR_FILE);
+                logMesg (logMesgBuf);
 
-	}
-      else
-	{
-	  char aFileName[MAX_PATH_LEN];
+                if (redirectFd (ECHKPNT_DEFAULT_OUTPUT_FILE, 2) == -1) {
+                    sprintf (logMesgBuf, "%s : redirect stderr to %s file fail\n%s\n", __func__, ECHKPNT_DEFAULT_OUTPUT_FILE, errno ? strerror (errno) : "");
+                    goto Error;
+                }
+            }
+        }
 
+        lMaxfds = sysconf (_SC_OPEN_MAX);
+        for( unsigned int ii = 3; ii < lMaxfds; ii++) {
+            close (ii);
+        }
 
+        sprintf (progName, "%s.%s", ECHKPNT_PROGRAM, pMethodName);
+        cargv[0] = progName;
+        freeTable_ ();
 
-	  if ((getChkpntDirFile (aFileName, ECHKPNT_STDOUT_FILE) == -1)
-	      || (redirectFd (aFileName, 1) == -1))
-	    {
-	      sprintf (logMesgBuf, "%s : redirect the stdout to %s fail\n",
-		       __func__, ECHKPNT_STDOUT_FILE);
-	      logMesg (logMesgBuf);
+        execv (echkpntProgPath, cargv); // we are not supposed  to come back from here
 
-	      if (redirectFd (ECHKPNT_DEFAULT_OUTPUT_FILE, 1) == -1)
-		{
-		  sprintf (logMesgBuf,
-			   "%s : redirect stdout to %s file fail\n%s\n",
-			   __func__, ECHKPNT_DEFAULT_OUTPUT_FILE,
-			   errno ? strerror (errno) : "");
-		  goto Error;
-		}
-	    }
+        sprintf (logMesgBuf, "%s : the child process execute the %s fail\n", __func__, progName);
+        fprintf (stderr, "%s", logMesgBuf);
+        logMesg (logMesgBuf);
+        closeLog ();
+        exit (-1);
+    }
 
-	  if ((getChkpntDirFile (aFileName, ECHKPNT_STDERR_FILE) == -1)
-	      || (redirectFd (aFileName, 2) == -1))
-	    {
-	      sprintf (logMesgBuf, "%s : redirect the stderr to %s fail\n",
-		       __func__, ERESTART_STDERR_FILE);
-	      logMesg (logMesgBuf);
+    while ((iChildPid = waitpid (iChildPid, &iStatus, 0)) < 0 && errno == EINTR) {
+        ; // wait for the child to be done
+    }
 
-	      if (redirectFd (ECHKPNT_DEFAULT_OUTPUT_FILE, 2) == -1)
-		{
-		  sprintf (logMesgBuf,
-			   "%s : redirect stderr to %s file fail\n%s\n",
-			   __func__, ECHKPNT_DEFAULT_OUTPUT_FILE,
-			   errno ? strerror (errno) : "");
-		  goto Error;
-		}
-	    }
-	}
-
-      lMaxfds = sysconf (_SC_OPEN_MAX);
-      for (ii = 3; ii < lMaxfds; ii++)
-	{
-	  close (ii);
-	}
-
-      sprintf (progName, "%s.%s", ECHKPNT_PROGRAM, pMethodName);
-      cargv[0] = progName;
-      freeTable_ ();
-
-      execv (echkpntProgPath, cargv);
-      sprintf (logMesgBuf, "%s : the child process execute the %s fail\n",
-	       __func__, progName);
-      fprintf (stderr, "%s", logMesgBuf);
-      logMesg (logMesgBuf);
-      closeLog ();
-      exit (-1);
+    if (iChildPid < 0) {
+        sprintf (logMesgBuf, "%s : %s fail, \n%s\n", __func__, "waitpid", errno ? strerror (errno) : "");
+        // goto Error;
+        fprintf (stderr, "%s", logMesgBuf);
+        logMesg (logMesgBuf);
+        freeTable_ ();
+        closeLog ();
+        exit (-1);
+    }
+    else {
+        if (WEXITSTATUS (iStatus) != 0) {
+            sprintf (logMesgBuf, "%s : the echkpnt.%s fail,the exit value is %d\n", __func__, pMethodName, WEXITSTATUS (iStatus)); // the english is fail, too
+            fprintf (stderr, "%s", logMesgBuf);
+            logMesg (logMesgBuf);
+            freeTable_ ();
+            closeLog ();
+            exit (WEXITSTATUS (iStatus));
+        }
     }
 
 
-
-
-  while ((iChildPid = waitpid (iChildPid, &iStatus, 0)) < 0
-	 && errno == EINTR);
-
-  if (iChildPid < 0)
-    {
-      sprintf (logMesgBuf, "%s : %s fail, \n%s\n",
-	       __func__, "waitpid", errno ? strerror (errno) : "");
-      goto Error;
+    pJobID = getenv (ECHKPNT_JOBID);
+    if (pJobID == NULL) {
+        writeEchkpntVar (ECHKPNT_OLD_JOBID, "");
+        sprintf (logMesgBuf, "%s : getenv() can not get the env variable LSB_JOBID\n", __func__);
+        logMesg (logMesgBuf);
     }
-  else
-    {
-      if (WEXITSTATUS (iStatus) != 0)
-	{
-	  sprintf (logMesgBuf,
-		   "%s : the echkpnt.%s fail,the exit value is %d\n", __func__,
-		   pMethodName, WEXITSTATUS (iStatus));
-	  fprintf (stderr, "%s", logMesgBuf);
-	  logMesg (logMesgBuf);
-	  freeTable_ ();
-	  closeLog ();
-	  exit (WEXITSTATUS (iStatus));
-	}
+    else  {
+        writeEchkpntVar (ECHKPNT_OLD_JOBID, pJobID);
     }
 
+    freeTable_ ();
+    closeLog ();
+    exit (0);
 
-  pJobID = getenv (ECHKPNT_JOBID);
-  if (pJobID == NULL)
-    {
-      writeEchkpntVar (ECHKPNT_OLD_JOBID, "");
-      sprintf (logMesgBuf,
-	       "%s : getenv() can not get the env variable LSB_JOBID\n",
-	       __func__);
-      logMesg (logMesgBuf);
-    }
-  else
-    {
-      writeEchkpntVar (ECHKPNT_OLD_JOBID, pJobID);
-    }
-  freeTable_ ();
-  closeLog ();
-  exit (0);
+// Error:
 
-Error:
-
-  fprintf (stderr, "%s", logMesgBuf);
-  logMesg (logMesgBuf);
-  freeTable_ ();
-  closeLog ();
-  exit (-1);
-
+//     fprintf (stderr, "%s", logMesgBuf);
+//     logMesg (logMesgBuf);
+//     freeTable_ ();
+//     closeLog ();
+//     exit (-1);
 }
