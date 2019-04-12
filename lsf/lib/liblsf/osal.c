846d8f442e7f9dc8751ad1da437212695a0becea
George Marselis <george.marselis@ncmm.uio.no>
2019-04-12 10:53:53 +0200

    osal.c: compiles with no warnings. moving on to pamd_globals.c

--- lsf/lib/liblsf/osal.c
+++ lsf/lib/liblsf/osal.c
@@ -21,47 +21,56 @@
 #include "lib/lib.h"
 #include "lib/lproto.h"
 #include "lib/osal.h"
+#include "lib/channel.h"
+
+
+void osal_c_bullshit( void )
+{
+    assert( chanMaxSize );
+    assert( lserrno );
+    assert( INFINIT_LOAD );
+    return;
+}
+
 
 int
-osInit_ (void)
+osInit_( void )
 {
-  static char first = TRUE;
-  if (first)
-    {
-      chanInit_ ();
-      first = FALSE;
+    static char first = TRUE;
+    if (first) {
+        chanInit_ ();
+        first = FALSE;
     }
-  return (0);
+
+    return 0;
 }
 
 char *
-osPathName_ (char *pathname)
+osPathName_ (char *pathname) // FIXME FIXME FIXME FIXME where are you going with this? investigate and delete if not used
 {
-  return (pathname);
+    return pathname;
 }
 
 char *
 osHomeEnvVar_ (void)
 {
-  return (getenv ("HOME"));
+    return getenv ("HOME"); // FIXME FIXME FIXME FIXME put this in configure.ac
 }
 
 void
-osConvertPath_ (char *pathname)
+osConvertPath_ (char *pathname) // FIXME FIXME FIXME FIXME FIXME use strsubstitute function
 {
-  register int i;
-  for (i = 0; pathname[i] != '\0' && i < MAX_PATH_LEN; i++)
-    {
-      if (pathname[i] == '\\')
-	pathname[i] = '/';
+    for( register unsigned int i = 0; pathname[i] != '\0' && i < MAX_PATH_LEN; i++) {
+        if (pathname[i] == '\\') {
+            pathname[i] = '/';
+        }
     }
-  return;
 
+    return;
 }
 
 int
-osProcAlive_ (int pid)
+osProcAlive_( pid_t pid)
 {
-  return (kill (pid, 0));
-
+    return (kill (pid, 0));
 }
