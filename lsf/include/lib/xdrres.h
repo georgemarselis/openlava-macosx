ab52315314a1fcb8d8bb769d6aeae3382a88a86f
George Marselis <george.marselis@ncmm.uio.no>
2019-08-21 15:49:01 +0200

    xdrres.c: clean. One more file!

--- lsf/include/lib/xdrres.h
+++ lsf/include/lib/xdrres.h
@@ -18,31 +18,31 @@
 
 #pragma once
 
-#ifndef __XDR_HEADER__
 // #include <rpc/xdr.h>
-#include "tirpc/rpc/xdr.h"
-#endif
+#include <tirpc/rpc/xdr.h>
 
-#include "daemons/libresd/resd.h"
-// #include "lib/lproto.h"
-#include "lib/header.h"
 #include "lib/rf.h"
 
-bool_t xdr_resConnect (XDR *, struct resConnect *, struct LSFHeader *);
-int xdr_resCmdBill (XDR *, struct resCmdBill *, struct LSFHeader *);
-int xdr_resSetenv (XDR *, struct resSetenv *, struct LSFHeader *);
-int xdr_resRKill (XDR *, struct resRKill *, struct LSFHeader *);
-int xdr_resGetpid (XDR *, struct resPid *, struct LSFHeader *);
-bool_t xdr_resGetRusage (XDR *, struct resRusage *, struct LSFHeader *);
-int xdr_resChdir (XDR *, struct resChdir *, struct LSFHeader *);
-int xdr_resControl (XDR *, struct resControl *, struct LSFHeader *);
+// #define LSF_SEEK_SET 0x1
+// #define LSF_SEEK_CUR 0x2
+// #define LSF_SEEK_END 0x4
+enum LSF_SEEK {
+	LSF_SEEK_SET = 0x1,
+	LSF_SEEK_CUR = 0x2,
+	LSF_SEEK_END = 0x4,
+	LSF_SEEK_NUL = 0xF00
+};
 
-int xdr_resStty (XDR *, struct resStty *, struct LSFHeader *);
-int xdr_niosConnect (XDR *, struct niosConnect *, struct LSFHeader *);
-int xdr_niosStatus (XDR *, struct niosStatus *, struct LSFHeader *);
-int xdr_resSignal (XDR *, struct resSignal *, struct LSFHeader *);
-
-bool_t xdr_ropenReq (XDR *, struct ropenReq *, struct LSFHeader *);
-bool_t xdr_rrdwrReq (XDR *, struct rrdwrReq *, struct LSFHeader *);
-bool_t xdr_rlseekReq (XDR *, struct rlseekReq *, struct LSFHeader *);
-bool_t xdr_noxdr (XDR *xdrs, size_t size, struct LSFHeader *hdr);
+/* xdrres.c */
+bool_t xdr_resCmdBill  ( XDR *xdrs, struct resCmdBill *cmd,      struct LSFHeader *hdr );
+bool_t xdr_resSetenv   ( XDR *xdrs, struct resSetenv *envp,      struct LSFHeader *hdr );
+bool_t xdr_resRKill    ( XDR *xdrs, struct resRKill *rkill,      struct LSFHeader *hdr );
+bool_t xdr_resGetpid   ( XDR *xdrs, struct resPid *pidreq,       struct LSFHeader *hdr );
+bool_t xdr_resGetRusage( XDR *xdrs, struct resRusage *rusageReq, struct LSFHeader *hdr );
+bool_t xdr_resChdir    ( XDR *xdrs, struct resChdir *ch,         struct LSFHeader *hdr );
+bool_t xdr_resControl  ( XDR *xdrs, struct resControl *ctrl,     struct LSFHeader *hdr );
+bool_t xdr_resStty     ( XDR *xdrs, struct resStty *tty,         struct LSFHeader *hdr );
+bool_t xdr_ropenReq    ( XDR *xdrs, struct ropenReq *req,        struct LSFHeader *hdr );
+bool_t xdr_rrdwrReq    ( XDR *xdrs, struct rrdwrReq *req,        struct LSFHeader *hdr );
+bool_t xdr_rlseekReq   ( XDR *xdrs, struct rlseekReq *req,       struct LSFHeader *hdr );
+bool_t xdr_noxdr       ( XDR *xdrs, size_t size,                 struct LSFHeader *hdr );
--- lsf/lib/liblsf/xdrres.c
+++ lsf/lib/liblsf/xdrres.c
@@ -25,47 +25,48 @@
 // #include "lib/lproto.h"
 #include "lib/rf.h"
 #include "lib/xdrres.h"
+#include "lib/xdrmisc.h"
 #include "lib/term.h"
+#include "lib/misc.h"
 
 bool_t
 xdr_resCmdBill (XDR *xdrs, struct resCmdBill *cmd, struct LSFHeader *hdr)
 {
-    char *sp;
-    int argc = 0;
+    char *sp = NULL;
+    unsigned int argc = 0;
     int nlimits = 0;
 
     if (!(xdr_portno (xdrs, &cmd->retport) &&
           xdr_int (xdrs, &cmd->rpid) &&
-          xdr_int (xdrs, &cmd->filemask) &&
+          xdr_u_int (xdrs, &cmd->filemask) &&
           xdr_int (xdrs, &cmd->priority) && xdr_int (xdrs, &cmd->options)))
     {
-        return (FALSE);
+        return FALSE;
     }
 
     sp = cmd->cwd;
     if (xdrs->x_op == XDR_DECODE) {
-        sp[0] = '\0';
+        sp = NULL;
     }
 
     if (!xdr_string (xdrs, &sp, MAX_PATH_LEN)) {
-        return (FALSE);
+        return FALSE;
     }
 
     if (xdrs->x_op == XDR_ENCODE) {
         for (argc = 0; cmd->argv[argc]; argc++);
     }
 
-    if (!xdr_int (xdrs, &argc)) {
-        return (FALSE);
+    if (!xdr_u_int (xdrs, &argc)) {
+        return FALSE;
     }
 
     if (xdrs->x_op == XDR_DECODE) {
-        assert( argc >= 0 );
-        if ((cmd->argv = (char **) calloc ( (unsigned long)argc + 1, sizeof (char *))) == NULL)
-            return (FALSE);
+        if ((cmd->argv = calloc ( argc + 1, sizeof (char *) ) ) == NULL)
+            return FALSE;
     }
 
-    for (int i = 0; i < argc; i++) {
+    for ( unsigned int i = 0; i < argc; i++) {
 
         if (!xdr_var_string (xdrs, &(cmd->argv[i]))) {
 
@@ -76,7 +77,7 @@ xdr_resCmdBill (XDR *xdrs, struct resCmdBill *cmd, struct LSFHeader *hdr)
                 }
                 free (cmd->argv);
             }
-            return (FALSE);
+            return FALSE;
         }
     }
 
@@ -89,10 +90,15 @@ xdr_resCmdBill (XDR *xdrs, struct resCmdBill *cmd, struct LSFHeader *hdr)
 
     if (!xdr_int (xdrs, &nlimits)) {
         if (xdrs->x_op == XDR_DECODE) {
-            goto DecodeQuit;
+            // goto DecodeQuit;
+            for ( unsigned int i = 0; i < argc; i++) {
+                free (cmd->argv[i]);
+            }
+            free (cmd->argv);
+            return FALSE;
         }
         else {
-            return (FALSE);
+            return FALSE;
         }
     }
 
@@ -104,30 +110,35 @@ xdr_resCmdBill (XDR *xdrs, struct resCmdBill *cmd, struct LSFHeader *hdr)
 
         if (!xdr_arrayElement (xdrs, (char *) &(cmd->lsfLimits[i]), hdr, xdr_lsfLimit)) {
             if (xdrs->x_op == XDR_DECODE) {
-                goto DecodeQuit;
+                // goto DecodeQuit;
+                for ( unsigned int i = 0; i < argc; i++) {
+                    free (cmd->argv[i]);
+                }
+                free (cmd->argv);
+                return FALSE;
             }
             else {
-                return (FALSE);
+                return FALSE;
             }
         }
     }
 
-    return (TRUE);
+    return TRUE;
 
-DecodeQuit:
-    for ( int i = 0; i < argc; i++) {
-        free (cmd->argv[i]);
-    }
-    free (cmd->argv);
-    return (FALSE);
+// DecodeQuit:
+//     for ( int i = 0; i < argc; i++) {
+//         free (cmd->argv[i]);
+//     }
+//     free (cmd->argv);
+//     return FALSE;
 }
 
 
 bool_t
 xdr_resSetenv (XDR *xdrs, struct resSetenv *envp, struct LSFHeader *hdr)
 {
-    char *sp;
-    int nenv = 0;
+    char *sp = NULL;
+    unsigned int nenv = 0;
 
     assert( hdr->length );
 
@@ -137,25 +148,24 @@ xdr_resSetenv (XDR *xdrs, struct resSetenv *envp, struct LSFHeader *hdr)
         }
     }
 
-    if (!xdr_int (xdrs, &nenv)) {
-        return (FALSE);
+    if (!xdr_u_int (xdrs, &nenv)) {
+        return FALSE;
     }
 
     if (xdrs->x_op == XDR_DECODE) {
-        assert( nenv >= 0 );
-        if ((envp->env = (char **) calloc ( (unsigned long)(nenv + 1), sizeof (char *))) == NULL) {
-            return (FALSE);
+        if ((envp->env = calloc ( nenv + 1, sizeof (char *))) == NULL) {
+            return FALSE;
         }
     }
 
-    for (int i = 0; i < nenv; i++) {
+    for (unsigned int i = 0; i < nenv; i++) {
         if (xdrs->x_op == XDR_DECODE) {
             if (!xdr_var_string (xdrs, &envp->env[i])) {
                 while (i--) {
                     free (envp->env[i]);
                 }
                 free (envp->env);
-                return (FALSE);
+                return FALSE;
             }
         }
         else {
@@ -165,7 +175,7 @@ xdr_resSetenv (XDR *xdrs, struct resSetenv *envp, struct LSFHeader *hdr)
                 sp = chDisplay_ (sp);
             }
             if (!xdr_var_string (xdrs, &sp)) {
-                return (FALSE);
+                return FALSE;
             }
         }
     }
@@ -174,43 +184,44 @@ xdr_resSetenv (XDR *xdrs, struct resSetenv *envp, struct LSFHeader *hdr)
         envp->env[nenv] = NULL;
     }
 
-    return (TRUE);
+    return TRUE;
 }
 
 bool_t
 xdr_resRKill (XDR *xdrs, struct resRKill *rkill, struct LSFHeader *hdr)
 {
     assert( hdr->length );
-    if (!(xdr_int (xdrs, &rkill->rid) && xdr_int (xdrs, &rkill->whatid) && xdr_int (xdrs, &rkill->signal))) {
-        return (FALSE);
+    if (!(xdr_u_long (xdrs, &rkill->rtaskid) && xdr_int (xdrs, &rkill->whatid) && xdr_int (xdrs, &rkill->signal))) {
+        return FALSE;
     }
 
-    return (TRUE);
+    return TRUE;
 }
 
 bool_t
 xdr_resGetpid (XDR *xdrs, struct resPid *pidreq, struct LSFHeader *hdr)
 {
     assert( hdr->length );
-    if (!(xdr_int (xdrs, &pidreq->rpid) && xdr_int (xdrs, &pidreq->pid))) {
-        return (FALSE);
+    // if (!(xdr_int (xdrs, &pidreq->rtaskid) && xdr_int (xdrs, &pidreq->pid))) {
+    if ( !xdr_u_long (xdrs, &pidreq->rtaskid) ) {
+        return FALSE;
     }
 
-    return (TRUE);
+    return TRUE;
 }
 
 bool_t
 xdr_resGetRusage (XDR *xdrs, struct resRusage *rusageReq, struct LSFHeader *hdr)
 {
     assert( hdr->length );
-    if (!(xdr_int (xdrs, &rusageReq->rid)
+    if (!(xdr_int (xdrs, &rusageReq->rtaskid)
           && xdr_int (xdrs, &rusageReq->whatid)
           && xdr_int (xdrs, &rusageReq->options)))
     {
-        return (FALSE);
+        return FALSE;
     }
 
-    return (TRUE);
+    return TRUE;
 }
 
 
@@ -221,15 +232,15 @@ xdr_resChdir (XDR *xdrs, struct resChdir *ch, struct LSFHeader *hdr)
 
     assert( hdr->length );
     if (xdrs->x_op == XDR_DECODE) {
-        sp[0] = '\0';
+        sp = NULL;
     }
 
     if (!xdr_string (xdrs, &sp, MAX_FILENAME_LEN)) {
-        return (FALSE);
+        return FALSE;
     }
 
 
-    return (TRUE);
+    return TRUE;
 }
 
 
@@ -238,9 +249,9 @@ xdr_resControl (XDR *xdrs, struct resControl *ctrl, struct LSFHeader *hdr)
 {
     assert( hdr->length );
     if (!(xdr_int (xdrs, &ctrl->opCode) && xdr_int (xdrs, &ctrl->data))) {
-        return (FALSE);
+        return FALSE;
     }
-    return (TRUE);
+    return TRUE;
 }
 
 
@@ -250,12 +261,12 @@ xdr_resStty (XDR *xdrs, struct resStty *tty, struct LSFHeader *hdr)
     assert( hdr->length );
     if (xdrs->x_op == XDR_ENCODE) {
         if (!encodeTermios_ (xdrs, &tty->termattr)) {
-            return (FALSE);
+            return FALSE;
         }
     }
     else {
         if (!decodeTermios_ (xdrs, &tty->termattr)) {
-            return (FALSE);
+            return FALSE;
         }
     }
 
@@ -265,32 +276,33 @@ xdr_resStty (XDR *xdrs, struct resStty *tty, struct LSFHeader *hdr)
           xdr_u_short (xdrs, &tty->ws.ws_xpixel) &&
           xdr_u_short (xdrs, &tty->ws.ws_ypixel)))
     {
-        return (FALSE);
+        return FALSE;
     }
 
-    return (TRUE);
+    return TRUE;
 }
 
 bool_t
 xdr_ropenReq (XDR *xdrs, struct ropenReq *req, struct LSFHeader *hdr)
 {
-    int flags        = 0;
-    char *sp         = req->fn;
-    unsigned int len = 0;
+    int flags           = 0;
+    char *sp            = req->fn;
+    size_t length       = 0;
 
 
     assert( hdr->length );
     if (xdrs->x_op == XDR_DECODE) {
-        len = MAX_FILENAME_LEN;
-        sp[0] = '\0';
+        length = MAX_FILENAME_LEN;
+        sp = NULL;
     }
     else {
         assert( strlen (req->fn) + 1 <= UINT_MAX );
-        len = (unsigned int)strlen (req->fn) + 1;
+        length = strlen (req->fn) + 1;
     }
 
-    if (!xdr_string (xdrs, &sp, len)) {
-        return (FALSE);
+    assert( length <= UINT_MAX );
+    if (!xdr_string (xdrs, &sp, (unsigned int) length)) {
+        return FALSE;
     }
 
     if (xdrs->x_op == XDR_ENCODE) {
@@ -306,7 +318,7 @@ xdr_ropenReq (XDR *xdrs, struct ropenReq *req, struct LSFHeader *hdr)
             flags |= LSF_O_NDELAY;
         }
 #endif
-#ifdef O_NONBLOCK
+#ifdef O_NONBLOCK // FIXME FIXME FIXME add to configure.ac
         if (req->flags & O_NONBLOCK) {
             flags |= LSF_O_NONBLOCK;
         }
@@ -323,7 +335,7 @@ xdr_ropenReq (XDR *xdrs, struct ropenReq *req, struct LSFHeader *hdr)
         if (req->flags & O_EXCL) {
             flags |= LSF_O_EXCL;
         }
-#ifdef O_NOCTTY
+#ifdef O_NOCTTY // FIXME FIXME FIXME add to configure.ac
         if (req->flags & O_NOCTTY) {
             flags |= LSF_O_NOCTTY;
         }
@@ -332,12 +344,12 @@ xdr_ropenReq (XDR *xdrs, struct ropenReq *req, struct LSFHeader *hdr)
             flags |= LSF_O_CREAT_DIR;
         }
         if (!xdr_int (xdrs, &flags)) {
-            return (FALSE);
+            return FALSE;
         }
     }
     else {
         if (!xdr_int (xdrs, &flags)) {
-            return (FALSE);
+            return FALSE;
         }
         req->flags = 0;
         if (flags & LSF_O_WRONLY) {
@@ -346,12 +358,12 @@ xdr_ropenReq (XDR *xdrs, struct ropenReq *req, struct LSFHeader *hdr)
         if (flags & LSF_O_RDWR) {
             req->flags |= O_RDWR;
         }
-#ifdef O_NDELAY
+#ifdef O_NDELAY // FIXME FIXME FIXME add to configure.ac
         if (flags & LSF_O_NDELAY) {
             req->flags |= O_NDELAY;
         }
 #endif
-#ifdef O_NONBLOCK
+#ifdef O_NONBLOCK // FIXME FIXME FIXME add to configure.ac
         if (flags & LSF_O_NONBLOCK) {
             req->flags |= O_NONBLOCK;
         }
@@ -368,7 +380,7 @@ xdr_ropenReq (XDR *xdrs, struct ropenReq *req, struct LSFHeader *hdr)
         if (flags & LSF_O_EXCL) {
             req->flags |= O_EXCL;
         }
-#ifdef O_NOCTTY
+#ifdef O_NOCTTY // FIXME FIXME FIXME add to configure.ac
         if (flags & LSF_O_NOCTTY) {
             req->flags |= O_NOCTTY;
         }
@@ -378,28 +390,26 @@ xdr_ropenReq (XDR *xdrs, struct ropenReq *req, struct LSFHeader *hdr)
         }
     }
     
-    if (!xdr_int (xdrs, &req->mode)) {
-        return (FALSE);
+    if (!xdr_u_int (xdrs, &req->mode)) {
+        return FALSE;
     }
     
-    return (TRUE);
+    return TRUE;
 }
 
 bool_t
 xdr_rrdwrReq (XDR *xdrs, struct rrdwrReq *req, struct LSFHeader *hdr)
 {
     assert( hdr->length );
-    assert( req->len <= INT_MAX );
-    return (xdr_int (xdrs, &req->fd) && xdr_int (xdrs, (int *)&req->len));
+    assert( req->length <= INT_MAX );
+    return xdr_int (xdrs, &req->fd) && xdr_u_long (xdrs, &req->length);
 }
 
 bool_t
 xdr_rlseekReq (XDR *xdrs, struct rlseekReq *req, struct LSFHeader *hdr)
 {
     int whence = 0;
-#define LSF_SEEK_SET 0x1
-#define LSF_SEEK_CUR 0x2
-#define LSF_SEEK_END 0x4
+
     assert( hdr->length );
     
     if (xdrs->x_op == XDR_ENCODE) {
@@ -407,7 +417,7 @@ xdr_rlseekReq (XDR *xdrs, struct rlseekReq *req, struct LSFHeader *hdr)
             whence |= LSF_SEEK_SET; // FIXME FIXME FIXME why will this never get executed?
         }
         if (req->whence & SEEK_CUR) {
-            whence |= LSF_SEEK_CUR;
+            whence |= LSF_SEEK_CUR; // enum LSF_SEEK in lib/xdrres.h
         }
         if (req->whence & SEEK_END) {
             whence |= LSF_SEEK_END;
@@ -415,10 +425,8 @@ xdr_rlseekReq (XDR *xdrs, struct rlseekReq *req, struct LSFHeader *hdr)
     }
 
     assert( req->offset <= INT_MAX );
-    if (!(xdr_int (xdrs, &req->fd) && xdr_int (xdrs, &whence) &&
-          xdr_int (xdrs, (int *)&req->offset)))
-    {
-        return (FALSE);
+    if (!(xdr_int (xdrs, &req->fd) && xdr_int (xdrs, &whence) && xdr_long (xdrs, &req->offset))) {
+        return FALSE;
     }
     
     if (xdrs->x_op == XDR_DECODE) {
@@ -434,7 +442,7 @@ xdr_rlseekReq (XDR *xdrs, struct rlseekReq *req, struct LSFHeader *hdr)
         }
     }
     
-    return (TRUE);
+    return TRUE;
 }
 
 bool_t
@@ -442,6 +450,8 @@ xdr_noxdr (XDR *xdrs, size_t size, struct LSFHeader *hdr)
 {
     assert( hdr->length );
     assert( size + LSF_HEADER_LEN <= INT_MAX );
-    (*(xdrs)->x_ops->x_setpostn)(xdrs, (unsigned int)(size + LSF_HEADER_LEN));
-    return (TRUE);
+    // (*(xdrs)->x_ops->x_setpostn)(xdrs, (unsigned int)(size + LSF_HEADER_LEN));
+    (*(xdrs)->x_ops->x_setpostn)(xdrs, (unsigned int) (size + LSF_HEADER_LEN)); // NOFIX, XDR wants to keep allocations small incase remote system runs out of memory
+    return TRUE;
 }
+    
